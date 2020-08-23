a6a8d055724a11c35d8300dce16f580b3a83e98a
dshrops1 <dustin.t.shropshire@gmail.com>
2020-08-22 19:25:35 -0600

    added basic component as I learn how kivy and python works

--- TruckLoaderApp.py
+++ TruckLoaderApp.py
@@ -1,1 +1,17 @@
 #main app
+import kivy
+kivy.require("1.11.1")
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from components import onStart
+
+class MainWidget(App):
+
+    def build(self):
+        return Widget()
+
+
+if __name__ == '__main__':
+    onStart.onOpeningSequence()
+    MainWidget().run()
---
+++ components/onStart.py
@@ -0,0 +1,11 @@
+import kivy
+kivy.require("1.11.1")
+
+from kivy.uix.image import Image
+
+#this later may be extened in a more generic sense to provide a general loading
+#currently a function may need to be a class later
+
+
+def onOpeningSequence():
+    return print("test that this works") #Image(source="/Users/dustinshropshire/Downloads/forkliftclipart.png")
